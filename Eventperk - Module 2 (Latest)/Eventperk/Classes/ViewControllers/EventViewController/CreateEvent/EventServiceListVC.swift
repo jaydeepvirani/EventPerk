5626974803e386a5912832b52c510b94ce6d5793
Jaydeep Virani <jaydeepvirani@gmail.com>
2017-08-06 13:20:44 +0530

    — Service Icon —

--- Eventperk - Module 2 (Latest)/Eventperk/Classes/ViewControllers/EventViewController/CreateEvent/EventServiceListVC.swift
+++ Eventperk - Module 2 (Latest)/Eventperk/Classes/ViewControllers/EventViewController/CreateEvent/EventServiceListVC.swift
@@ -14,6 +14,19 @@ class EventServiceListVC: UIViewController {
     @IBOutlet var btnSave: UIButton!
     @IBOutlet var tblService: UITableView!
     
+    @IBOutlet var viewServicesIcon: UIView!
+    
+    //MARK: Dragable View
+    var snapX:CGFloat = 40.0 /// must be >= 1.0
+    var snapY:CGFloat = 40.0 /// must be >= 1.0
+    
+    var threshold:CGFloat = 0.0 /// how far to move before dragging
+    
+    var selectedView:UIView? /// the guy we're dragging
+    
+    var shouldDragY = true /// drag in the Y direction?
+    var shouldDragX = true /// drag in the X direction?
+    
     //MARK: Other Objects
     var dictCreateEventDetail = NSMutableDictionary()
     var arrServiceList = NSMutableArray()
@@ -33,6 +46,24 @@ class EventServiceListVC: UIViewController {
     //MARK:- Initialization
     func initialization() {
         btnSave.layer.cornerRadius = 10
+        
+        var myview = ServicesView()
+        viewServicesIcon.addSubview(myview)
+        myview.frame = CGRect(x: 10, y: 40, width: 40, height: 40)
+        
+        myview = ServicesView()
+        viewServicesIcon.addSubview(myview)
+        myview.frame = CGRect(x: 60, y: 80, width: 40, height: 40)
+        myview.fillColor = UIColor(red: 1.0, green: 0.5, blue: 1.0, alpha: 1.0).cgColor
+        
+        setupGestures()
+    }
+    
+    func setupGestures() {
+        let pan = UIPanGestureRecognizer(target:self, action:#selector(self.pan(_:)))
+        pan.maximumNumberOfTouches = 1
+        pan.minimumNumberOfTouches = 1
+        self.view.addGestureRecognizer(pan)
     }
     
     //MARK:- Button TouchUp
@@ -62,6 +93,59 @@ class EventServiceListVC: UIViewController {
     func tableView(_ tableView: UITableView, didSelectRowAtIndexPath indexPath: IndexPath) {
         tblService.tag = indexPath.row
         self.performSegue(withIdentifier: "eventSubServiceListSegue", sender: nil)
+    }
+    
+    //MARK:- Gesture
+    
+    func pan(_ rec:UIPanGestureRecognizer) {
+        
+        let p:CGPoint = rec.location(in: self.view)
+        var center:CGPoint = .zero
+        
+        switch rec.state {
+        case .began:
+            print("began")
+            selectedView = view.hitTest(p, with: nil)
+            if selectedView != nil {
+                self.view.bringSubview(toFront: selectedView!)
+            }
+            
+        case .changed:
+            if let subview = selectedView {
+                center = subview.center
+                let distance = sqrt(pow((center.x - p.x), 2.0) + pow((center.y - p.y), 2.0))
+                print("distance \(distance) threshold \(threshold)")
+                
+                if subview is MyView {
+                    if distance > threshold {
+                        if shouldDragX {
+                            subview.center.x = p.x - (p.x.truncatingRemainder(dividingBy: snapX))
+                        }
+                        if shouldDragY {
+                            subview.center.y = p.y - (p.y.truncatingRemainder(dividingBy: snapY))
+                        }
+                    }
+                }
+            }
+            
+        case .ended:
+            print("ended")
+            if let subview = selectedView {
+                if subview is MyView {
+                    // do whatever
+                }
+            }
+            selectedView = nil
+            
+        case .possible:
+            print("possible")
+        case .cancelled:
+            print("cancelled")
+            selectedView = nil
+        case .failed:
+            print("failed")
+            selectedView = nil
+        }
     }
     
     //MARK:- Service Array
